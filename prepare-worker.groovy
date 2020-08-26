2e913af97345859eb14192c045f625674df08464
Bakuliya <abdimazhitova00@gmail.com>
2020-08-25 19:27:24 -0500

    updates

--- prepare-worker.groovy
+++ prepare-worker.groovy
@@ -1,15 +1,13 @@
 node {
-    withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-ssh-key', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
+    withCredentials([sshUserPrivateKey(credentialsId: 'Jenkins-master-ssh-key', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
         stage('Init') {
-            sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY root@157.245.5.147 yum install epel-release -y'
+            sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@157.245.5.147 yum install epel-release -y'
         }
         stage("Install git") {
-            sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY root@157.245.5.147 yum install git -y'
+            sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@157.245.5.147 yum install git -y'
         }
         stage("Install Java"){
-            sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY root@157.245.5.147 yum install java-1.8.0-openjdk-devel -y'
+            sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@157.245.5.147 yum install java-1.8.0-openjdk-devel -y'
         }
     }
-
-
 }
